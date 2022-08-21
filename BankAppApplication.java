8cf5afafc6bac07b4deb869879cd6d44b0543874
David <74435367+dmatarinl@users.noreply.github.com>
2022-08-21 20:42:37 +0200

    Add files via upload

---
+++ BankAppApplication.java
@@ -0,0 +1,24 @@
+package io.bankapp;
+
+    /*Comment Section */
+//Importing Spring framework main applications + SpringBoot
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BankAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BankAppApplication.class, args);
+
+	}
+
+}
+
+//
+//
+// @author dmatarinl
+//
+//
